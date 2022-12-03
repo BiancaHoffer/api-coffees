ac0e0fc702b6ed69d71930587c39819e3d4b1baa
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-12-02 21:36:53 -0300

    teste cors 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 var cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:3000/', 
+    origin:'https://coffee-delivery-by-biancahoffer.vercel.app/', 
     //credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
