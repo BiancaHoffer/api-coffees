4fcf3512e0efd2901e88974a5d88fd82e4d28d8c
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-12-02 21:30:26 -0300

    teste cors 3

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 var cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
+    origin:'http://localhost:3000/', 
+    //credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 
